8c0c3fbc86a298d9bf64ab0bed2d789bc61b0f56
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-12 15:04:04 +0700

    feat: add title and icon description

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import 'package:dicost/detail_screen.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -11,26 +13,74 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'DICOST',
       theme: ThemeData(
         primarySwatch: Colors.blue,
       ),
-      home: HomeScreen(),
+      home: DetailScreen(),
     );
   }
 }
 
-class HomeScreen extends StatelessWidget {
-  const HomeScreen({Key? key}) : super(key: key);
+class DetailScreen extends StatelessWidget {
+  const DetailScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: Text('DICOST'),
-      ),
-      body: Center(
-        child: Text("Hello World"),
+      body: SafeArea(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: [
+            Image.asset('images/farm-house.jpg'),
+            Container(
+                margin: EdgeInsets.all(16.0),
+                child: Text(
+                  'DICOST',
+                  textAlign: TextAlign.center,
+                  style: TextStyle(
+                    fontSize: 30.0,
+                    fontWeight: FontWeight.bold
+                  ),
+                )
+            ),
+            Container(
+              margin: EdgeInsets.symmetric(vertical: 16.0),
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                children: [
+                  Column(
+                    children: [
+                      Icon(
+                          Icons.location_on
+                      ),
+                      SizedBox(height: 8.0),
+                      Text('Salemba')
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                          Icons.monetization_on
+                      ),
+                      SizedBox(height: 8.0),
+                      Text('Rp 2.400.000')
+                    ],
+                  ),
+                  Column(
+                    children: [
+                      Icon(
+                          Icons.location_city
+                      ),
+                      SizedBox(height: 8.0),
+                      Text('Jakarta')
+                    ],
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
       )
     );
   }
