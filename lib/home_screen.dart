9286669780aa29c01a32606257127d270cfcb2d6
Naufal Fadhil Athallah <naufalfadhilathallah@gmail.com>
2022-07-14 18:02:44 +0700

    refactor(home): refactoring for dynamic view multiplatform

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -68,61 +68,69 @@ class HomeScreen extends StatelessWidget {
                 )
               ),
               Expanded(
-                child: GridView.builder(
-                  padding: EdgeInsets.only(bottom: 16, left: 8, right: 8),
-                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
-                    crossAxisCount: 2,
-                  ),
-                  itemBuilder: (context, index) {
-                    final CostData cost = costDataList[index];
-                    return InkWell(
-                      onTap: () {
-                        Navigator.push(context, MaterialPageRoute(builder: (context) {
-                          return DetailScreen(cost: cost);
-                        }));
-                      },
-                      child: Card(
-                        margin: EdgeInsets.all(10),
-                        elevation: 3,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: BorderRadius.circular(20),
-                        ),
-                        child: Column(
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: [
-                            Expanded(
-                                flex: 4,
-                                child: ClipRRect(
-                                  borderRadius: BorderRadius.only(
-                                      topLeft: Radius.circular(20),
-                                      topRight: Radius.circular(20)
-                                  ),
-                                  child: Image.asset(cost.imageAsset),
-                                )
-                            ),
-                            Expanded(
-                                flex: 2,
-                                child: Padding(
-                                  padding: EdgeInsets.all(8),
-                                  child: Column(
-                                    crossAxisAlignment: CrossAxisAlignment.start,
-                                    mainAxisSize: MainAxisSize.min,
-                                    children: [
-                                      Text(
-                                        cost.name,
-                                        style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-                                      ),
-                                      Text(cost.location + ", " + cost.city)
-                                    ],
-                                  ),
-                                )
-                            )
-                          ],
-                        ),
+                child: LayoutBuilder(
+                  builder: (BuildContext context, BoxConstraints constraints) {
+                    return GridView.builder(
+                      padding: EdgeInsets.only(bottom: 16, left: 8, right: 8),
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                        crossAxisCount: (constraints.maxWidth <= 720) ? 2 : (constraints.maxWidth <= 1080) ? 4 : 6,
                       ),
+                      itemBuilder: (context, index) {
+                        final CostData cost = costDataList[index];
+                        return InkWell(
+                          onTap: () {
+                            Navigator.push(context, MaterialPageRoute(builder: (context) {
+                              return DetailScreen(cost: cost);
+                            }));
+                          },
+                          child: Card(
+                            margin: EdgeInsets.all(10),
+                            elevation: 3,
+                            shape: RoundedRectangleBorder(
+                              borderRadius: BorderRadius.circular(20),
+                            ),
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Expanded(
+                                    flex: 4,
+                                    child: ClipRRect(
+                                      borderRadius: BorderRadius.only(
+                                          topLeft: Radius.circular(20),
+                                          topRight: Radius.circular(20)
+                                      ),
+                                      child: Image.asset(cost.imageAsset),
+                                    )
+                                ),
+                                Expanded(
+                                    flex: 2,
+                                    child: Padding(
+                                      padding: EdgeInsets.all(8),
+                                      child: Column(
+                                        crossAxisAlignment: CrossAxisAlignment.start,
+                                        mainAxisSize: MainAxisSize.min,
+                                        children: [
+                                          Text(
+                                            cost.name,
+                                            style: TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+                                            overflow: TextOverflow.ellipsis,
+                                          ),
+                                          Text(
+                                              "${cost.location} ${cost.city}",
+                                              overflow: TextOverflow.ellipsis,
+                                          )
+                                        ],
+                                      ),
+                                    )
+                                )
+                              ],
+                            ),
+                          ),
+                        );
+                      },
+                      itemCount: costDataList.length,
                     );
                   },
-                  itemCount: costDataList.length,
                 ),
               )
             ],
